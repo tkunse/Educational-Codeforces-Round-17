764fd4985bac799301d9ee46d2ac6f9ae8fe99c7
tkunse <tkunse_retoac@sina.com>
2017-02-01 14:57:16 +0800

    Create B4_test.cpp

---
+++ otherRes/B4_test.cpp
@@ -0,0 +1,78 @@
+// GNU C++ 14
+// Learning C++ with real code
+// Test: http://codeforces.com/contest/762/problem/B
+
+#include <bits/stdc++.h>
+
+#define mp(a, b) make_pair((a), (b))
+#define pb(a) push_back((a))
+#define pf(a) push_front((a))
+#define rb() pop_back()
+#define rf() pop_front()
+#define sz(a) ((int)a.size())
+
+using namespace std;
+
+typedef long long lld;                // lld means long long
+typedef pair<int, int> pii;
+typedef pair<lld, lld> pll;
+typedef pair<lld, int> pli;
+typedef pair<int, lld> pil;
+typedef vector<vector<int>> Matrix;
+typedef vector<vector<int>> Adj;
+typedef vector<int> Row;
+typedef complex<double> Complex;
+typedef vector<Complex> Vcomplex;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const lld LINF = 1e18;
+const double FINF = 1e15;
+const double EPS = 1e-9;
+const double PI = 2.0 * acos(0.0);
+
+int a, b, c;
+int n;
+
+int main() {
+  scanf("%d %d %d %d", &a, &b, &c, &n);
+  vector<pii> v(n);
+  for (int i = 0; i < n; ++i) {
+    int x;
+    char s[10];
+    scanf(" %d %s ", &x, s);
+    if (s[0] == 'U') {
+      v[i] = pii(x, 0);
+    } else {
+      v[i] = pii(x, 1);
+    }
+  }
+  sort(v.begin(), v.end());
+  lld sum = 0;
+  int ans = 0;
+  for (int i = 0; i < v.size(); ++i) {
+    if (v[i].second == 0) {
+      if (a > 0) {
+        --a;
+        ++ans;
+        sum += v[i].first;
+      } else if (c > 0) {
+        --c;
+        ++ans;
+        sum += v[i].first;
+      }
+    } else {
+      if (b > 0) {
+        --b;
+        ++ans;
+        sum += v[i].first;
+      } else if (c > 0) {
+        --c;
+        ++ans;
+        sum += v[i].first;
+      }
+    }
+  }
+  printf("%d %lld\n", ans, sum);
+}
+
